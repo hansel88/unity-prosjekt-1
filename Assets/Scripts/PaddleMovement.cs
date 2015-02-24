5e3aec61c4e4bc18a5de08d5dfb2af62626d3b3d
hansel88 <hpn_x@hotmail.com>
2015-02-24 04:27:20 -0800

    .

---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleMovement : MonoBehaviour {
+
+
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("LEFT!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("RIGHT!");
+        }
+         */
+
+        transform.Translate(1f * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
+
+	}
+}
